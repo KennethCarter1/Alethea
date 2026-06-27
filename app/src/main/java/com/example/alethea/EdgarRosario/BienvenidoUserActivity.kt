14d99eba65ad2f9139372cb20fdbd62b7a374879
Edgar Rosario <edg507@outlook.com>
2026-06-26 23:36:05 -0500

    Agregando Front del menu usuario

--- app/src/main/java/com/example/alethea/EdgarRosario/BienvenidoUserActivity.kt
+++ app/src/main/java/com/example/alethea/EdgarRosario/BienvenidoUserActivity.kt
@@ -1,21 +1,12 @@
 package com.example.alethea.EdgarRosario
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.alethea.R
 
 class BienvenidoUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_edgar_bienvenido_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
