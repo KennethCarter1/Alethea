c333ddcf6ecf14535e572a30cdf21ad8026b3b54
Edgar Rosario <edg507@outlook.com>
2026-06-28 13:45:59 -0500

    Pantalla iniciar sesion,inicio y registro

--- app/src/main/java/com/example/alethea/EdgarRosario/InicioActivity.kt
+++ app/src/main/java/com/example/alethea/EdgarRosario/InicioActivity.kt
@@ -1,21 +1,21 @@
 package com.example.alethea.EdgarRosario
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.alethea.R
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_edgar_inicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+
+        val btnIniciarSesion = findViewById<LinearLayout>(R.id.btnIniciarSesion)
+        btnIniciarSesion.setOnClickListener {
+            val intent = Intent(this, IniciarSesionActivity::class.java)
+            startActivity(intent)
         }
     }
 }
